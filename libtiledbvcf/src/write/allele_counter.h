986d77c6f597dc584eef3f09e63c5ff782943c98
George Powley <george.powley@gmail.com>
2022-03-07 18:35:35 -0500

    working

--- libtiledbvcf/src/write/allele_counter.h
+++ libtiledbvcf/src/write/allele_counter.h
@@ -10,6 +10,7 @@
 
 namespace tiledb::vcf {
 
+// Future expansion of ingestion tasks
 class IngestionTask {};
 
 class AlleleCounter : public IngestionTask {
@@ -38,26 +39,33 @@ class AlleleCounter : public IngestionTask {
   void finalize();
 
  private:
-  inline static const std::string AC_ALLELE = "allele";
-  inline static const std::string AC_COUNT = "count";
   inline static const std::string AC_URI = "allele_count";
+  inline static const std::string AC_CONTIG = "contig";  // dim: str
+  inline static const std::string AC_POS = "pos";        // dim: int
+  inline static const std::string AC_ALLELE = "allele";  // attr: str
+  inline static const std::string AC_COUNT = "count";    // attr: int
 
   std::shared_ptr<Context> ctx_ = nullptr;
   std::unique_ptr<Array> array_ = nullptr;
   std::map<std::string, int> allele_count_;
-  std::string locus_;
+  std::string contig_;
+  uint32_t pos_;
 
+  // Buffers for tiledb write
+  std::string ac_contig_;
+  std::vector<uint64_t> ac_contig_offsets_;
+  std::vector<int32_t> ac_pos_;
   std::string ac_allele_;
   std::vector<uint64_t> ac_allele_offsets_;
   std::vector<int32_t> ac_count_;
 
-  // reusable htslib buffer for bcf_get_* functions
+  // Reusable htslib buffer for bcf_get_* functions
   int* dst_ = nullptr;
 
-  // reusable htslib buffer size for bcf_get_* functions
+  // Reusable htslib buffer size for bcf_get_* functions
   int ndst_ = 0;
 
-  // Update results with data at the previous locus.
+  // Update results with data at the previous locus
   void update_results();
 };
 
